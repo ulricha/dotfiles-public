f160de7a62bde19f7faeda24b708a090f8b65c58
Alexander Ulrich <alex@etc-network.de>
2013-12-22 10:16:19 +0100

    xmonad stuff

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -4,19 +4,31 @@ import XMonad
 import XMonad.Layout.Fullscreen
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Hooks.DynamicLog
 import XMonad.Config.Xfce
+import XMonad.Util.EZConfig
+import XMonad.Layout.ResizableTile
+import XMonad.Hooks.SetWMName
 
 main = do
     xmonad $ xfceConfig
-        { modMask = mod4Mask
+        { modMask = myModMask
         , terminal = "urxvt" 
         , layoutHook = myLayout
-        }
+        , startupHook = setWMName "LG3D"
+        } `additionalKeys` myKeyBindings
 
 myLayout = smartBorders $ avoidStruts (
     Full |||
     Tall 1 (3/100) (1/2) |||
+    ResizableTall 1 (3/100) (1/2) [] |||
     Mirror (Tall 1 (3/100) (1/2))) |||
     noBorders (fullscreenFull Full)
+
+myModMask = mod4Mask
+
+myKeyBindings = 
+    [ ((myModMask, xK_s), spawn "synapse")
+    , ((myModMask, xK_b), sendMessage ToggleStruts)
+    , ((myModMask, xK_a), sendMessage MirrorShrink)
+    , ((myModMask, xK_z), sendMessage MirrorExpand)
+    ]
